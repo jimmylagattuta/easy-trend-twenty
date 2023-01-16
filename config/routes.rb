aeb96bbead78d4c53937f22e448f5177c7c3a6a2
james <jimmy.lagattuta@gmail.com>
2023-01-16 13:26:19 -0800

    root to added to keep on homescreen, still bugfixing browser router losing public index.html and showing white screen and console on refresh on userhome

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
   # Defines the root path route ("/")
+  root to: "/homescreen"
   namespace :api do
     namespace :v1 do
       resources :notes, only: [:index]
