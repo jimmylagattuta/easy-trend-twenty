13a5a54613306fd4ed1d48c0454337ca16335cc0
james <jimmy.lagattuta@gmail.com>
2023-01-16 22:57:05 -0800

    try to go back

---
+++ es20client/src/components/SettingsHome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class SettingsHome extends React.Component {
+
+	render() {
+		console.log('SettingsHome props', this.props);
+		return <div><h1>SettingsHome</h1></div>;
+	}
+}
+
+export default SettingsHome;
