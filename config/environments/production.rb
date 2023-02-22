3f7f244ee3cd2d2eca231b277b72963cf3872570
james <jimmy.lagattuta@gmail.com>
2023-02-21 18:45:50 -0800

    running emailer through heroku to test if it will work, error in the word errors, should have seen it right away in the error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,8 +23,8 @@ Rails.application.configure do
 
 
 
-
-config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_caching = false
   # Code is not reloaded between requests.
   config.cache_classes = true
 
